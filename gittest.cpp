1eac0bfe889bbe10a5880875cefb3a2a478a6dc0
sguki <seunguk2da@jbnu.ac.kr>
2023-08-13 16:13:32 +0900

    first commit

--- gittest.cpp
+++ gittest.cpp
@@ -7,3 +7,12 @@
 //conflict 방지로 하나하나 올리는게 낫다고 함
 //git status 로 상태를 알수 있다. 
 //git commit -m "first commit" 으로 히스토리를 만들어준다 
+//만든 히스토리를  github에 보내기 위해 remote add origin을 사용
+//ex) git remote add origin https://github.com/sguki/myfirstgit.git
+//이제 git hub와 내 vscode의 연결고리가 생김
+//git push origin master로 코드를 마스터 브랜치로 보낸다
+
+//첫 커밋에 git push origin master 이후 에러 발생
+//에러 원인: 로컬에 readme 파일이 없는데 원격 저장소에만 있을때 생긴다
+//해결 방법 :git pull origin main
+//입력후 다시 git add

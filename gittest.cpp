f6fb09ecf05b7c8fed2e2cd2d2155d9b582e28f5
sguki <seunguk2da@jbnu.ac.kr>
2023-08-13 16:05:05 +0900

    first commit

---
+++ gittest.cpp
@@ -0,0 +1,7 @@
+//깃 커밋 테스트
+//vscode 터미널에 bash로 들어가서
+//첫 커밋에 git init
+//git add .
+//git에 add . 을 하면 전부 올린다는 것
+//git 에 gittest.cpp만 올리고 싶다면 git add gittest.cpp 를 사용
+//conflict 방지로 하나하나 올리는게 낫다고 함
